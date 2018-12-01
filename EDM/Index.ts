5f8bbabb546828fc97222b5cb4d9e3c2ab849e20
vlad <mikefaraday7@gmail.com>
2018-12-01 20:28:57 +0300

    улучшил paging

--- EDM/Index.ts
+++ EDM/Index.ts
@@ -92,6 +92,8 @@ class Index extends Control {
         var len = this.allItems.length;
         if (len % this.sizePage == 0) {
             this.page--;
+            this.page--; // ???
+            this.page++; // ???
         }
         this.changeCurrentPage(this.page);
         this._forceUpdate();
