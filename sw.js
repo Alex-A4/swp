64ccfdddc1581891ad00876b52001d5d302e9062
Alex <alex.A4.25@yandex.ru>
2018-12-02 02:40:33 +0300

    Fixed sw.js

--- sw.js
+++ sw.js
@@ -8,37 +8,7 @@ self.addEventListener('install', (event) => {
          .then(function(cache) {
             console.log('Opened cache');
             return cache.addAll(['/',
-               '/w.js',
-               '/WS.Core/css/core-min.css',
-               '/all_desktop.css',
-               '/WS.Core/ext/requirejs/bundles.js',
-               '/cdn/requirejs/2.3.5-p3/require-min.js',
-               '/contents.min.js',
-               '/WS.Core/ext/requirejs/config.js',
-               '/WS.Core/lib/Ext/tslib.min.js',
-               '/WS.Core/core/i18n.min.js',
-               '/WS.Core/ext/requirejs/plugins/i18n.min.js',
-               '/WS.Core/ext/requirejs/plugins/text.min.js',
-               '/WS.Core/ext/requirejs/plugins/native-css.min.js',
-               '/WS.Core/ext/requirejs/plugins/tmpl.min.js',
-               '/WS.Core/ext/requirejs/plugins/is.min.js',
-               '/WS.Core/core/polyfill/PromiseAPIDeferred.min.js',
-               '/WS.Core/ext/requirejs/plugins/wml.min.js',
-               '/WS.Core/ext/requirejs/plugins/is-api.min.js',
-               '/WS.Core/core/Control.min.tmpl',
-               '/WS.Core/ext/requirejs/plugins/css.min.js',
-               '/WS.Core/core/TimeTester.min.tmpl',
-               '/Controls/Utils/getWidth.css',
-               '/Controls/Utils/getWidth.min.css',
-               '/Controls/Button/Close.css',
-               '/Controls/Popup/Templates/Stack/StackTemplate.css',
-               '/Controls/Button/Close.css',
-               '/EDM/Document/DocumentStyles.css',
-               '/EDM/Reestr/rowstyle.min.css',
-               '/EDM/indexstyle.min.css',
-               '/EDM/Reestr/cross.png',
-               '/EDM/Reestr/rowstyle.css',
-               '/EDM/indexstyle.css']);
+               '/w.js']);
          })
    );
 });
@@ -53,7 +23,7 @@ self.addEventListener('fetch', (event) => {
       });
    } if (event.request.url.indexOf('/api/') > -1) {
       return fetch(event.request);
-   }
+   } else {
    event.respondWith(
       caches.match(event.request)
          .then(function(response) {
@@ -76,4 +46,5 @@ self.addEventListener('fetch', (event) => {
             );
          })
    );
+      }
 });
