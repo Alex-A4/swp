c3878524e22ac1b7788d67fad841593b783ed5aa
Alex <alex.A4.25@yandex.ru>
2018-12-01 20:48:33 +0300

    Fixed !== null

--- EDM/LocalStorage/Source.ts
+++ EDM/LocalStorage/Source.ts
@@ -59,11 +59,11 @@ const LocalStorageWorker = {
                 if (pref[i] === null)
                     continue;
 
-                if (pref[i].title !== null)
+                if (pref[i].title)
                     if (pref[i].title.toLowerCase().indexOf(line.toLowerCase()) !== -1)
                         temp.push(pref[i]);
     
-                if (pref[i].description !== null)
+                if (pref[i].description)
                     if (pref[i].description.toLowerCase().indexOf(line.toLowerCase()) !== -1)
                         temp.push(pref[i]);
             }
