e07ce2c92214347f5f9d633575b483804896d11c
Alex <alex.A4.25@yandex.ru>
2018-12-02 01:07:42 +0300

    Fixed localhost

--- EDM/Index.ts
+++ EDM/Index.ts
@@ -110,7 +110,7 @@ class Index extends Control {
     }
 
     protected _afterMount(): void {
-        Socket.startListen('ws://localhost:8080');
+        Socket.startListen(`ws://${location.hostname}:8888`);
     }
 
     private addButtonClickHandler(e: Event, data: Object): void {
