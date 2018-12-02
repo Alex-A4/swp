f446fc7f238c18c928774fba4eae177991559189
Alex <alex.A4.25@yandex.ru>
2018-12-02 06:05:47 +0300

    Wrap into the string

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get("/api/read", (req, res) => {
 
 app.post("/api/delete", function (req, res) {
    DBWorker.delete(req.query.id.replace(/"/g, ''))
-      .then((status) => res.sendStatus(status) && sendAll('delete', req.query.id.replace(/"/g, ''), {}))
+      .then((status) => res.sendStatus(status) && sendAll('delete', '"'+req.query.id.replace(/"/g, '')+'"', {}))
       .catch((err) => res.status(500) && res.send(err));
 });
 
